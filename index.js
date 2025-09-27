7c6830522afd32156e4f1eaa24ec67a94071f7a7
anshjain216 <anshj5501@gmail.com>
2025-09-27 15:06:15 +0530

    deleted the repeated code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const chat = require("./models/chat");
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
 const methodOverride = require("method-override");
